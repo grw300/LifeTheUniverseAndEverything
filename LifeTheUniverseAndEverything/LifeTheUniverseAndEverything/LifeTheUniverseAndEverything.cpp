e8740bbb95041523711030ef2e891d4a297d10cb
grw300 <grw300@gmail.com>
2014-09-23 21:55:09 -0500

    added accepted solution

---
+++ LifeTheUniverseAndEverything/LifeTheUniverseAndEverything/LifeTheUniverseAndEverything.cpp
@@ -0,0 +1,18 @@
+#include <cassert>
+#include <string>
+#include <iostream>
+
+using namespace std;
+
+int main () {
+
+    int a;
+
+    while(cin >> a){
+		if (a == 42)
+			break;
+        cout << a << endl;
+    }
+    
+    return 0;
+}
